970bb6f44c9bf2435727c99168484faae74a7ff3
Marwan8766 <marwanyasser344@gmail.com>
2023-07-21 19:49:51 +0200

    added categories

--- controllers/authController.js
+++ controllers/authController.js
@@ -298,6 +298,9 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   });
   if (!user) return next(new AppError('There is no user with this email'), 404);
 
+  if (user.emailConfirmOtpExpires < Date.now())
+    return next(new AppError("Your OTP hasn't expired yet", 400));
+
   // GENERATE the random reset otp
   await createSendOtpEmail(user);
 
