d69a5207ff3b336350b726583e1a7620ba6ee790
Marwan8766 <marwanyasser344@gmail.com>
2023-09-17 17:54:51 +0200

    removed server.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/appErrorController');
@@ -49,5 +51,45 @@ app.all('*', (req, res, next) => {
 
 // IMPLEMENTING a global error handling middleware
 app.use(globalErrorHandler);
+
+// server.js
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION, server is shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true, // that is what i added due to terminal error
+  })
+  .then(() => console.log('DB is connected...'));
+
+const port = process.env.PORT;
+
+const server = app.listen(port, `0.0.0.0`, () => {
+  console.log(`Server running on port ${port}...`);
+});
+server.timeout = 120000; // 120 seconds
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION, server is shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+///////////////////////////
 
 module.exports = app;
